4e81b1fec827964c2f89df79c8df66a4a7dde9b8
Patrick Biffle <pbiffle@gmail.com>
2018-11-29 10:35:44 -0700

    added delete/put queries and routes

--- queries.js
+++ queries.js
@@ -3,5 +3,26 @@ const db = require('./database-connection')
 module.exports = {
     getAll() {
         return db('students')
+    },
+    getById(id){
+        return db('students').where({id: id}).first()
+    },
+    getById2(id){
+        return db('students').where('id', id)
+    },
+    getById3(id){
+        return db.select().from('students').where('id', id)
+    },
+    createStudent(newStudent){
+        return db('students').insert(newStudent)
+    },
+    createStudent2(newStudent){
+        return db('students').insert(newStudent).returning('*')
+    },
+    deleteStudent(id){
+        return db('students').where('id', id).delete()
+    },
+    updateStudent(id, updatedStudentInfo){
+        return db('students').where('id', id).update(updatedStudentInfo).returning('*')
     }
 }
